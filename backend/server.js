6df4e3282268354a3b227731feb03562f81a4d7c
Francislie <mfrancislie@gmail.com>
2023-07-27 02:59:18 -0700

    password bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./model/User');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const salt = bcrypt.genSaltSync(10);
 
 mongoose.connect(
   'mongodb+srv://mern-blog-user:mern-blog-user@myblog.obdi4n0.mongodb.net/?retryWrites=true&w=majority'
@@ -16,7 +19,7 @@ app.post('/register', async (req, res) => {
   try {
     const userDoc = await User.create({
       username,
-      password,
+      password: bcrypt.hashSync(password,salt),
     });
     res.json(userDoc);
   } catch (e) {
